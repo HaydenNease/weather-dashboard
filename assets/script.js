6ecabe96b0fd5bb5191fe9eb768b5dfdd59d23e5
HaydenNease <hayden.nease@gmail.com>
2022-12-28 12:51:38 -0500

    weather and forecast fetch on click

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,27 @@ var key = "ac74bca302ee441e90b0b98a1190e465"
 var weatherApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + key;
 var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + key;
 
-q = "charlotte"
 
 var getWeather = function (data) {
   var lat = (data[0].lat);
   var lon = (data[0].lon);
-  var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
-  fetch(weatherUrl)
+  var weather = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
+  fetch(weather)
+  .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+  var getForecast = function (data) {
+    var lat = (data[0].lat);
+    var lon = (data[0].lon);
+    var forecast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
+    fetch(forecast)
     .then(function (response) {
       return response.json();
     })
@@ -18,19 +32,25 @@ var getWeather = function (data) {
     .catch(function (error) {
       console.log(error);
     })
-}
-var getCoordinates = function () {
-  var locationUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + q + "&appid=" + key;
-  fetch(locationUrl)
+  }
+  var getCoordinates = function (q) {
+    var locationUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + q + "&appid=" + key;
+    fetch(locationUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       getWeather(data);
+      getForecast(data);
     })
     .catch(function (error) {
       console.log(error);
     })
-}
-
-getCoordinates();
+  }
+  
+  // Search Button
+  $(document).on('click', '.btn-primary', function (event) {
+    event.preventDefault();
+    var q = $("#searchInput").val();
+    getCoordinates(q);
+  });
